5fe6bea09198588e8471b2c062d686ca7d7f801b
budisevener <budisevener@gmail.com>
2025-09-10 16:57:19 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Document ChatApp',
+  title: 'Document ChatApp v0.1',
   description: '',
   copyright: '',
   privacy_policy: '',
